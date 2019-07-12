fdd9cf45055af99f7264a645fc6a9fe59e7ffb64
Steve Golden <sgolden2525@outlook.com>
2019-07-12 03:28:09 -0500

    Game shape and hooks
    In process of getting the shape of Simon.
    
    Added react hook to the project.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  
+  const [buttonNum] = useState([1,2,3,4]);
+
   return (
     <div className="App">
       <h1>Simon Game</h1>
-      <section></section>
+      <div id="simonWrapper">
+      <section className="simon">
+          <div className="pad red" ></div>
+          <div className="pad green" ></div>
+          <div className="pad yellow" ></div>
+          <div className="pad blue" ></div>
+          <div className="display" id="display"></div>
+        </section>
+      </div>
     </div>
   );
 }
