b72494634bf6197e357e2ac70d05762cf167eac4
Steve Golden <sgolden2525@outlook.com>
2019-07-14 02:28:57 -0500

    Added level button
    Added level button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React, { useState } from 'react';
 
-import './App.css';
+import './App.css'
 
-function App() {
-  
-  const [buttonNum] = useState([1,2,3,4]);
-  const centerStyle = {margin: 'auto'};
-  return (
-    <div className="App">
-      <h1>Simon Game</h1>
-      <div id="simonWrapper">
-        <section className="simon" style={centerStyle}>
-          <div className="pad red" ></div>
-          <div className="pad green" ></div>
-          <div className="pad yellow" ></div>
-          <div className="pad blue" ></div>
-          <div className="display" id="display">
-            <div id="simonName">simon</div>
-            <div id="simonLevel">level</div>
-            <div id="startButton">start</div>
-          </div>
-        </section>
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      levelNum: 1,
+    }
+    this.setLevel = this.setLevel.bind(this);
+  }
+
+
+
+  setLevel(){
+    if (this.state.levelNum === 4) {
+      this.setState({
+        levelNum: 1,
+      })
+    } else {
+      this.setState({
+        levelNum: this.state.levelNum + 1,
+      })
+    }
+  }
+
+  render() {
+    const centerStyle = { margin: 'auto' };
+    return (
+      <div className="App">
+        <h1>Simon Game</h1>
+        <div id="simonWrapper">
+          <section className="simon" style={centerStyle}>
+            <div className="pad red" ></div>
+            <div className="pad green" ></div>
+            <div className="pad yellow" ></div>
+            <div className="pad blue" ></div>
+            <div className="display" id="display">
+              <div id="simonName">simon</div>
+              <button id="simonLevel" onClick={this.setLevel}>level {this.state.levelNum}</button>
+              <div id="startButton">start</div>
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+
 
 export default App;
